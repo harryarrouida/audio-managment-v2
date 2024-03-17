71a5debd6243832cd154375bae6edf293ceb64dc
harryarrouida <hamzaarrouida@gmail.com>
2024-03-17 20:35:47 +0000

    there is nothing we can do to protect you

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -21,7 +21,6 @@ const TYPES = ["Music", "Quran", "News", "Emission", "Causerie", "Fiction"];
 
 export default function Form() {
   const [formData, setFormData] = useState({});
-
   const [createAudio] = useMutation(CREATE_AUDIO);
 
   const handleChange = (e) => {
@@ -30,7 +29,6 @@ export default function Form() {
       ...prevState,
       [name]: value,
     }));
-    console.log(formData);
   };
 
   const handleSubmit = async (e) => {
@@ -46,6 +44,7 @@ export default function Form() {
       console.log("failed", error);
     }
   };
+
   return (
     <form onSubmit={handleSubmit}>
       <GeneralFields handleChange={handleChange} formData={formData} />
@@ -54,55 +53,50 @@ export default function Form() {
           name="type"
           onChange={handleChange}
           id="HeadlineAct"
-          class="w-3/5 mx-auto px-3 py-3  w-1/3 mt-1.5 rounded-lg border-gray-300 text-gray-700 sm:text-sm"
+          className="w-3/5 mx-auto px-3 py-3  w-1/3 mt-1.5 rounded-lg border-gray-300 text-gray-700 sm:text-sm"
         >
           <option selected disabled>
             select a type
           </option>
           {TYPES.map((type) => (
-            <option value={type}>{type}</option>
+            <option key={type} value={type}>
+              {type}
+            </option>
           ))}
         </select>
       </div>
 
-      {formData.type === "Music" ? (
-        <MusicFields handleChange={handleChange} />
-      ) : (
-        <></>
+      {formData.type && (
+        <>
+          {formData.type === "Music" && (
+            <MusicFields handleChange={handleChange} />
+          )}
+          {formData.type === "Emission" && (
+            <EmissionFields handleChange={handleChange} />
+          )}
+          {formData.type === "Quran" && (
+            <QuranFields handleChange={handleChange} />
+          )}
+          {formData.type === "News" && (
+            <NewsFields handleChange={handleChange} />
+          )}
+          {formData.type === "Fiction" && (
+            <FictionFields handleChange={handleChange} />
+          )}
+          {formData.type === "Causerie" && (
+            <CauserieFields handleChange={handleChange} />
+          )}
+        </>
       )}
-      {formData.type === "Emission" ? (
-        <EmissionFields handleChange={handleChange} />
-      ) : (
-        <></>
-      )}
-      {formData.type === "Quran" ? (
-        <QuranFields handleChange={handleChange} />
-      ) : (
-        <></>
-      )}
-      {formData.type === "News" ? (
-        <NewsFields handleChange={handleChange} />
-      ) : (
-        <></>
-      )}
-      {formData.type === "Fiction" ? (
-        <FictionFields handleChange={handleChange} />
-      ) : (
-        <></>
-      )}
-      {formData.type === "Causerie" ? (
-        <CauserieFields handleChange={handleChange} />
-      ) : (
-        <></>
-      )}
+
       <div className="mx-auto text-center">
         <button
-          class="my-5 group relative inline-block text-sm font-medium text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
+          className="my-5 group relative inline-block text-sm font-medium text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
           type="submit"
         >
-          <span class="absolute inset-0 translate-x-0 translate-y-0 bg-indigo-600 transition-transform group-hover:translate-x-0.5 group-hover:translate-y-0.5"></span>
+          <span className="absolute inset-0 translate-x-0 translate-y-0 bg-indigo-600 transition-transform group-hover:translate-x-0.5 group-hover:translate-y-0.5"></span>
 
-          <span class="relative block border border-current bg-white px-8 py-3">
+          <span className="relative block border border-current bg-white px-8 py-3">
             submit
           </span>
         </button>
