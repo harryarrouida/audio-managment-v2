927daed0c6ccda73ad2052819a2e2b0864e9d72c
harryarrouida <hamzaarrouida@gmail.com>
2024-03-17 16:13:57 +0000

    hello world

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import {Link} from "react-router-dom"
 
 export default function Home() {
   return (
     <div>
-      <div className='text-2xl'>Welcome</div>
+      <div className="w-2/4 mx-auto my-20 flex justify-center">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/3/39/Logo_SNRT.svg"
+          alt="not found"
+          className="w-1/4"
+        ></img>
+      </div>
+      <div className="w-2/4 mx-auto flex justify-evenly">
+        <Link to={"/create-audio"} className="btn">CREATE AUDIO</Link>
+        <Link to={"/fetch-audios"}>FETCH AUDIOS</Link>
+      </div>
     </div>
-  )
+  );
 }
